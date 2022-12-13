232a3fd43b6c57a62f588c32302fc14057ff6183
Lê Trung Hiếu - TCIS <hieult2@saigonnewport.com.vn>
2022-12-13 16:07:56 +0700

    TCIS.Training.Menthod

---
+++ TCIS.Training/TCIS.Training.Menthod/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCIS.Training.Menthod
+{
+    class Program
+    {
+        // Phương Thức
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+       
+        }
+
+        public void Prirt()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
