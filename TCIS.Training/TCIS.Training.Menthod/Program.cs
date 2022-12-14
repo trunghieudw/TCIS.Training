0565040e7c373f43578a8532ee235af56b343671
Lê Trung Hiếu - TCIS <hieult2@saigonnewport.com.vn>
2022-12-14 16:50:45 +0700

    [Method] Áp dụng vòng lặp, mảng

--- TCIS.Training/TCIS.Training.Menthod/Program.cs
+++ TCIS.Training/TCIS.Training.Menthod/Program.cs
@@ -21,45 +21,68 @@ namespace TCIS.Training.Menthod
                 return $"{Id} - {Name}";
             }
         }
+        #region dungnv2
+        //public class People
+        //{
+        //    public Person[] Persons = null;
+
+        //    public People 
+        //    {
+        //        Persons = new Person[];
+        //    }
+
+        //    public void Add(List<Person> person)
+        //    {
+        //        Persons.AddRange(person);  
+        //    } 
+
+        //    public void Add(Person person)
+        //    {
+        //        Persons.Add(person);  
+        //    }
+
+        //    public void Remove(int id) //==> Method parameter
+        //    {
+        //         var p = Persons.FirstOrDefault(x=>x.Id = id); //variable
+        //         Persons.Remove(p);
+        //    }
+
+        //    public void Remove(params int[] id)
+        //    {
+        //         var p = Persons.FirstOrDefault(x=>x.Id = id);
+        //         Persons.Remove(p);
+        //    }
 
 
-        public class People
+
+        //    //Update
+        //    //Find
+        //    //RemoveMultils
+
+
+        //}
+        #endregion
+
+        public class People2
         {
-            public Person[] Persons = null;
 
-            public People 
+            // tạo mảng con người có số lượng phần tử truyền vào
+            public People2(int number)
             {
-                Persons = new Person[];
+                int[] people2 = new int[number];
+                for (int i = 0; i < people2.Length; i++)
+                {
+                    Console.WriteLine($"Danh sách người đã tạo: {i + 1}");
+                }
             }
-
-            public void Add(List<Person> person)
+            // thêm đối tượng vào trong mảng
+            public void Add(int id, string name)
             {
-                Persons.AddRange(person);  
-            } 
-
-            public void Add(Person person)
-            {
-                Persons.Add(person);  
+                Person person = new Person(id,name);
+                person.Id = id;
+                person.Name = name;
+                Console.WriteLine($"Người bạn đã thêm vào có Id: {id} và Name: {name}");
             }
-
-            public void Remove(int id) //==> Method parameter
-            {
-                 var p = Persons.FirstOrDefault(x=>x.Id = id); //variable
-                 Persons.Remove(p);
-            }
-
-            public void Remove(params int[] id)
-            {
-                 var p = Persons.FirstOrDefault(x=>x.Id = id);
-                 Persons.Remove(p);
-            }
-
-
-
-            //Update
-            //Find
-            //RemoveMultils
-
 
         }
 
@@ -67,23 +90,33 @@ namespace TCIS.Training.Menthod
         static void Main(string[] args)
         {
             Console.OutputEncoding = Encoding.UTF8;
+            #region dungnv2
+            //People people = new People();
 
-            People people = new People();
+            //Person person = new Person(1,"A");
+            //people.Add(person);
 
-            Person person = new Person(1,"A");
-            people.Add(person);
+            //Person person2 = new Person(2, "B");
+            //people.Add(person2);
 
-            Person person2 = new Person(2, "B");
-            people.Add(person2);
+            //people.Remove(1,2,3,4)
 
-            people.Remove(1,2,3,4)
+            //for (int i = 0; i < people.Persons.Count(); i++)
+            //{
+            //    Person p = people.Persons[i];
 
-            for (int i = 0; i < people.Persons.Count(); i++)
-            {
-                Person p = people.Persons[i];
-
-                Console.WriteLine(p.ToString());
-            }
+            //    Console.WriteLine(p.ToString());
+            //}
+            #endregion
+            // Thực hiện sử dụng vòng lặp mảng, làm các yêu cầu:
+            // Thêm, Xoá, Cập nhật, Tìm kiếm, Xoá nhiều class People
+            // khởi tạo mảng con người
+            Console.WriteLine("Khởi tạo mảng con người");
+            Console.WriteLine("Nhập số lượng con người muốn tạo: ");
+            int amount = 0;
+            amount = Convert.ToInt32(Console.ReadLine());
+            People2 people2 = new People2(amount);
+            people2.Add(1, "a");
 
             Console.ReadKey();
         }
