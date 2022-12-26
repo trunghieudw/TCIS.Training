e953e746e1d8d5dc343ca0c9433755b6f4ef0260
Lê Trung Hiếu - TCIS <hieult2@saigonnewport.com.vn>
2022-12-26 14:31:02 +0700

    [TCIS.Training] Dummy data lớp học

--- TCIS.Training/CalculateTheScoresOfTheClasses/Program.cs
+++ TCIS.Training/CalculateTheScoresOfTheClasses/Program.cs
@@ -1,13 +1,54 @@
-﻿using System;
+﻿using CalculateTheScoresOfTheClasses.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace CalculateTheScoresOfTheClasses
 {
     class Program
     {
+        static List<Class> Classes { get; set; }
+
+
+        static void DummyData() // tạo dữ liệu giả
+        {
+            Classes = new List<Class>();
+
+
+
+            var classes = new List<string>{"6A1", "6A2", "6A3", "6A4", "6A5", "6A6",
+                "7A1", "7A2", "7A3", "8A1", "8A2", "8A3", "8A4", "9A1", "9A2", "9A3", "9A4"};
+
+
+            // generates data Class danh sách lớp
+            for (int i = 0; i < classes.Count(); i++)
+            {
+                var ex = new Class
+                {
+                    Id = i + 1,
+                    Name = classes[i]
+                };
+                Classes.Add(ex);
+
+            }
+        }
+
         static void Main(string[] args)
         {
             Console.OutputEncoding = Encoding.UTF8;
+            DummyData();
+            Console.WriteLine("=========================");
+            Console.WriteLine("DANH SÁCH LỚP");
+            foreach (var c in Classes)
+            {
+                Console.WriteLine(c.ToString());
+            }
+          
+
+
+
+            Console.ReadKey();
 
         }
     }
