58fb29acf809d0823b14875955276e7a820b4084
Lê Trung Hiếu - TCIS <hieult2@saigonnewport.com.vn>
2023-01-04 08:14:02 +0700

    [TCIS.Training] Thực hiện tạo class tính lương teacher

---
+++ TCIS.Training/CalculateTheScoresOfTheClasses/Entities/Teacher/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateTheScoresOfTheClasses.Entities.Teacher
+{
+    public abstract class Person
+    {
+        protected int Id { get; set; }
+        protected string Name { get; set; }
+    }
+}
---
+++ TCIS.Training/CalculateTheScoresOfTheClasses/Entities/Teacher/Teacher.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateTheScoresOfTheClasses.Entities.Teacher
+{
+    public class Teacher : Person
+    {
+        public Teacher(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public int TeachId => Id;
+        public string TeachName => Name;
+
+        public int Coefficient { get; set; }// hệ số
+        public double Allowance { get; set; }// phụ cấp
+        public override string ToString()
+        {
+            return $"{Id}-{Name}-{Coefficient}-{Allowance}";
+        }
+    }
+}
